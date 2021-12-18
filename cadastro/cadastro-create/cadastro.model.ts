210fc3c9a002f8e4758da7d60dd854b8ae5b13a1
Joao V. Salgado <joaovsalgado22@gmail.com>
2021-12-18 11:54:41 -0300

    final

---
+++ cadastro/cadastro-create/cadastro.model.ts
@@ -0,0 +1,29 @@
+{
+    "products": [
+      {
+        "id": 1,
+        "name": "Caneta BIC preta",
+        "price": 5.89
+      },
+      {
+        "id": 2,
+        "name": "pc",
+        "price": 12000
+      },
+      {
+        "name ": "geladeira",
+        "": "1913.99",
+        "id": 4
+      },
+      {
+        "name ": "geladeira",
+        "price": "1913.99",
+        "id": 5
+      },
+      {
+        "name ": "monitor tela plana",
+        "price": "20000",
+        "id": 6
+      }
+    ]
+  }
